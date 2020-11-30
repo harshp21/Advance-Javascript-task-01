5424625ee01a232122ba92e10df050f6d29090fe
Harsh Pawar <31727764+harshp21@users.noreply.github.com>
2020-11-30 16:50:06 +0530

    Add files via upload

---
+++ advance-javascript-task-01/script.js
@@ -0,0 +1,28 @@
+let countDownContainer = document.createElement('div');
+countDownContainer.setAttribute('class', 'count-down-container');
+
+let messageContainer = document.createElement('div');
+messageContainer.setAttribute('class', 'msg-container');
+
+//Count down Config.
+let countDownStart = 10;
+let countDownEnd = 0;
+let countDownMsg = 'Happy<br>New Year';
+
+//Counting down execution.
+for (let num = countDownStart; num > countDownEnd; num--) {
+    let timeout = 1000 * (10 - num);
+    setTimeout(() => {
+        countDownContainer.innerText = num;
+        if (num === 1) {
+            setTimeout(() => {
+                countDownContainer.style.display = 'none';
+                messageContainer.style.display = 'flex';
+                messageContainer.innerHTML = countDownMsg;
+            }, 1000);
+        }
+    }, timeout);
+}
+
+//appending container to body.
+document.body.append(countDownContainer, messageContainer);
